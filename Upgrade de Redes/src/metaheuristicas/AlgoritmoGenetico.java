77343bc01dc55cb0b3c18470990cf537439d8cb2
Cláudio Diego T. de Souza <souza.claudiodiego@gmail.com>
2018-07-27 00:10:41 -0300

    AlgoritmoGenetico: métodos cruzamento(), mutacao() e checar_viabilidade()

--- Upgrade de Redes/src/metaheuristicas/AlgoritmoGenetico.java
+++ Upgrade de Redes/src/metaheuristicas/AlgoritmoGenetico.java
@@ -10,60 +10,59 @@ public class AlgoritmoGenetico {
 	private Integer tamanho_populacao;
 	private Integer tamanho_nova_geracao;
 	private Integer tamanho_solucoes_elite;
+	private Double limite_custo;
 	private Individuo populacao [];
-	private Individuo nova_geracao [];
+	private Individuo nova_geracao []; // quantos novos individuos serão gerados?
 	private Individuo solucoes_elite []; // quantas soluções elite manteremos para o path relinking?
 	private Double prob_cruzamento = 0.7;
 	private Double prob_mutacao = 0.2;
-	private Integer max_filhos;
 	private Integer geracoes_sem_melhoria = 0;
 	
-	public AlgoritmoGenetico(Grafo grafo, Integer tamanho_populacao) {
+	public AlgoritmoGenetico(Grafo grafo, Integer tamanho_populacao, Double limite_custo) {
 		
-		init(grafo, tamanho_populacao);
+		init(grafo, tamanho_populacao, limite_custo);
 		
 		gerar_populacao_inicial();
 		
 		gerar_novos_individuos();
 	}
 	
-	public void init(Grafo grafo, Integer tamanho_populacao) {
+	public void init(Grafo grafo, Integer tamanho_populacao, Double limite_custo) {
 		this.grafo = grafo;
 		this.tamanho_populacao = tamanho_populacao;
 		this.tamanho_nova_geracao = tamanho_populacao / 2;
 		this.tamanho_solucoes_elite = tamanho_populacao / 2;
+		this.limite_custo = limite_custo;
 		this.populacao = new Individuo[tamanho_populacao];
-		this.nova_geracao = new Individuo[tamanho_populacao/2];
-		this.solucoes_elite = new Individuo[tamanho_populacao/2];
+		this.nova_geracao = new Individuo[tamanho_nova_geracao];
+		this.solucoes_elite = new Individuo[tamanho_solucoes_elite];
 	}
 	
 
 	public void gerar_populacao_inicial() {
-		for (int i = 0; i < this.tamanho_populacao; i++) {
-			populacao[i] = novo_individuo();
+		Integer individuos_gerados = 0;
+		while(individuos_gerados < tamanho_populacao) {
+			Individuo novo_individuo = novo_individuo();
+			if(checar_viabilidade(novo_individuo)) {
+				populacao[individuos_gerados++] = novo_individuo;
+			}
 		}
 	}
 	
 	public Individuo novo_individuo() {
+		
 		String id = new String();
+		
 		for (int i = 0; i < this.grafo.getTotal_vertices(); i++) {
+			
 			id = id + new Random().nextInt(2);
+			
 		}
-		Individuo individuo = new Individuo(id, custo_individuo(id));
+		
+		Individuo individuo = new Individuo(id, grafo);
+		
 		return individuo;
 	}
-	
-	public Double custo_individuo(String id) {
-		Grafo grafo_teste = Grafo.replica(grafo);
-		String nos [] = id.split("");
-		for(int i = 0; i < nos.length; i++) {
-			if(nos[i] == "1"){
-				grafo_teste.upgrade(String.valueOf(i));
-			}
-		}
-		return grafo_teste.getCusto();
-	}
-	
 	
 	public void gerar_novos_individuos() {
 		
@@ -75,21 +74,106 @@ public class AlgoritmoGenetico {
 				
 				Double rand_cruzamentos = Math.random();
 				Double rand_mutacao = Math.random();
+				
 				if(rand_cruzamentos <= prob_cruzamento) {
-					cruzamento();
+					
+					try {
+						
+						novos_individuos = cruzamento(novos_individuos);
+						
+					} catch (ArrayIndexOutOfBoundsException e) {
+						System.out.println("Quantidade de indivivuos gerados por cruzamento excede limite");
+					}
+					
 				}
+				
 				if(rand_mutacao <= prob_mutacao) {
-					mutacao();
+					
+					try {
+						
+						novos_individuos = mutacao(novos_individuos);
+						
+					} catch (ArrayIndexOutOfBoundsException e) {
+						System.out.println("Quantidade de indivivuos gerados por mutação excede limite");
+					}
+					
 				}
 			}
 		}
 	}
 		
-	public void cruzamento() {
+	public Integer cruzamento(Integer novos_individuos) {
 		
+		Integer rand_corte = new Random().nextInt(grafo.getTotal_vertices());
+		Integer rand_pai = new Random().nextInt(tamanho_populacao);
+		Integer rand_mae = new Random().nextInt(tamanho_populacao);
+		
+		while (rand_pai == rand_mae) {
+			rand_pai = new Random().nextInt(tamanho_populacao);
+			rand_mae = new Random().nextInt(tamanho_populacao);
+		}
+		
+		Individuo pai = populacao[rand_pai];
+		Individuo mae = populacao[rand_mae];
+		
+		String id_filho_1 = pai.getId().substring(0, rand_corte) + mae.getId().substring(rand_corte+1, grafo.getTotal_vertices());
+		String id_filho_2 = mae.getId().substring(0, rand_corte) + pai.getId().substring(rand_corte+1, grafo.getTotal_vertices());
+		
+		Individuo filho_1 = new Individuo(id_filho_1, grafo);
+		Individuo filho_2 = new Individuo(id_filho_2, grafo);
+		
+		if(checar_viabilidade(filho_1)) {
+			
+			nova_geracao[novos_individuos++] = filho_1;
+		
+		}
+		
+		if(checar_viabilidade(filho_2)) {
+			
+			nova_geracao[novos_individuos++] = filho_2;
+			
+		}
+		
+		return novos_individuos;
 	}
 	
-	public void mutacao() {
+	public Integer mutacao(Integer novos_individuos) {
 		
+		Integer rand_mutacao = new Random().nextInt(grafo.getTotal_vertices());
+		Integer rand_individuo = new Random().nextInt(tamanho_populacao);
+		
+		Individuo individuo = populacao[rand_individuo];
+		
+		Character alelo_x = individuo.getId().charAt(rand_mutacao);
+		Character alelo_y = null;
+		
+		if(alelo_x.equals('0')) {
+			alelo_y = '1';
+		}
+		else {
+			alelo_y = '0';
+		}
+		
+		String id_mutante = individuo.getId().substring(0, rand_mutacao-1) + alelo_y
+				+ individuo.getId().substring(rand_mutacao+1, grafo.getTotal_vertices());
+		
+		Individuo mutante = new Individuo(id_mutante, grafo);
+		
+		if(checar_viabilidade(mutante)) {
+			
+			nova_geracao[novos_individuos++] = mutante;
+			
+		}
+		
+		return novos_individuos;
+	}
+	
+	private Boolean checar_viabilidade(Individuo individuo) { // é preciso avaliar as demais restrições do problema
+		if(individuo.getCusto(grafo) <= this.limite_custo) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
--- Upgrade de Redes/src/metaheuristicas/Individuo.java
+++ Upgrade de Redes/src/metaheuristicas/Individuo.java
@@ -1,14 +1,16 @@
 package metaheuristicas;
 
+import grafos.Grafo;
+
 public class Individuo {
 	
 	private String id;
 	private Double custo;
 	private Boolean solucao_elite = false;
 	
-	public Individuo(String id, Double custo) {
+	public Individuo(String id, Grafo grafo) {
 		this.id = id;
-		this.custo = custo;
+		this.custo = getCusto(grafo);
 	}
 	
 	public String getId() {
@@ -17,10 +19,6 @@ public class Individuo {
 	
 	public void setId(String id) {
 		this.id = id;
-	}
-	
-	public Double getCusto() {
-		return custo;
 	}
 	
 	public void setCusto(Double custo) {
@@ -33,6 +31,26 @@ public class Individuo {
 	
 	public void setSolucao_elite(Boolean solucao_elite) {
 		this.solucao_elite = solucao_elite;
+	}
+	
+	public Double getCusto(Grafo grafo) {
+		
+		Grafo grafo_teste = Grafo.replica(grafo);
+		
+		String nos [] = id.split("");
+		
+		for(int i = 0; i < nos.length; i++) {
+			
+			if(nos[i] == "1"){
+				
+				grafo_teste.upgrade(String.valueOf(i));
+				
+			}
+		}
+		
+		this.custo = grafo_teste.getCusto();
+		
+		return custo;
 	}
 	
 	
